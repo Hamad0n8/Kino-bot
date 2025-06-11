5d25b186e24a8d0a03f8084ee7d0013c3aa04e5f
Hamad0n8 <botbaro38@gmail.com>
2025-06-11 16:56:20 +0500

    Update main.py

--- main.py
+++ main.py
@@ -630,7 +630,7 @@ def process_backup(msg):
 if __name__ == "__main__":
     # Барқарор кардани вебхук
     bot.remove_webhook()
-    bot.set_webhook(url=f"https://films-bot-9fxf.onrender.com/7947429084:AAECl4VTgRdgv53IAixvZ5qgDMvABI8_d0o")
+    bot.set_webhook(url=f"https://Kino-bot.onrender.com/TOKEN")
     
     # Оғози сервер Flask
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 11000)))
